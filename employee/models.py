c80399c810280caf191e1d6f0f25fb7423312b00
Satoshi Suzuki <earthkundesu@icloud.com>
2018-10-23 22:40:26 +0900

    Make Employee List

---
+++ employee/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Department(models.Model):
+	name = models.CharField('部署名', max_length=20)
+	created_at = models.DateTimeField('日付', default=timezone.now)
+	
+	def __str__(self): # 特殊メソッド　社員の名前を返すメソッド
+			return self.name
+
+
+class Employee(models.Model):
+	first_name = models.CharField('名', max_length=20)
+	last_name = models.CharField('姓', max_length=20)
+	email = models.EmailField('メールアドレス', blank=True)
+	department = models.ForeignKey(
+		Department, verbose_name='部署', on_delete=models.PROTECT,
+	)
+	created_at = models.DateTimeField('日付', default=timezone.now)
+	
+	def __str__(self):
+		# self.departmentはオブジェクトを渡しているが、Department class で再帰的に特殊メソッドを返して、文字を返している。
+		return '{0} {1} {2}'.format(self.first_name, self.last_name, self.department.name)
