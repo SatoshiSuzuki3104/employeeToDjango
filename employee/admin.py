3745f4ad9ed216c8f780d19c1bf0f5a751ab5f3a
Satoshi Suzuki <earthkundesu@icloud.com>
2018-10-23 23:33:58 +0900

    Add Club Model

--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Department, Employee
+from .models import Department, Employee, Club
 
 
 admin.site.register(Department)
 admin.site.register(Employee)
+admin.site.register(Club)
 
--- employee/models.py
+++ employee/models.py
@@ -10,6 +10,14 @@ class Department(models.Model):
 			return self.name
 
 
+class Club(models.Model):
+	name = models.CharField('部活名', max_length=20)
+	created_at = models.DateTimeField('日付', default=timezone.now)
+	
+	def __str__(self):
+		return self.name
+
+
 class Employee(models.Model):
 	first_name = models.CharField('名', max_length=20)
 	last_name = models.CharField('姓', max_length=20)
@@ -17,7 +25,11 @@ class Employee(models.Model):
 	department = models.ForeignKey(
 		Department, verbose_name='部署', on_delete=models.PROTECT,
 	)
-	created_at = models.DateTimeField('日付', default=timezone.now)
+	club = models.ManyToManyField(
+		Club, verbose_name='部活',
+	)
+	created_at = models.DateTimeField('日付', default=timezone.now),
+	# address = models.CharField('住所', max_length=20, blank=True)
 	
 	def __str__(self):
 		# self.departmentはオブジェクトを渡しているが、Department class で再帰的に特殊メソッドを返して、文字を返している。
